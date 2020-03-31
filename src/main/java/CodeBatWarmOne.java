63a5a4eba25a54b4bac7ee6fbbb771ac5d89b427
Grant Houston <grant.houston44@hotmail.com>
2020-03-31 11:16:08 +0100

    icyHot

--- src/main/java/CodeBatWarmOne.java
+++ src/main/java/CodeBatWarmOne.java
@@ -156,6 +156,11 @@ public class CodeBatWarmOne {
 
     }
 
+    public boolean icyHot(int temp1, int temp2) {
+        return ((temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0));
+    }
+
+
 
 
 }
